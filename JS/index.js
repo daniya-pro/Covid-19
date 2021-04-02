5863ec7646237aedcd0f0502750767f128cf1e07
daniya-pro <syedadaniyahaider@gmail.com>
2021-04-03 02:00:36 +0500

    updated

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,43 @@
-fetch("https://services9.arcgis.com/N9p5hsImWXAccRNI/arcgis/rest/services/Nc2JKvYFoAEOFCG5JSI6/FeatureServer/2/query?f=json&where=Recovered%3C%3E0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Recovered%20desc&resultOffset=0&resultRecordCount=250&resultType=standard&cacheHint=true", {
+fetch(
+  "https://services9.arcgis.com/N9p5hsImWXAccRNI/arcgis/rest/services/Nc2JKvYFoAEOFCG5JSI6/FeatureServer/2/query?f=json&where=Recovered%3C%3E0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Recovered%20desc&resultOffset=0&resultRecordCount=250&resultType=standard&cacheHint=true", {
+  referrer: "https://www.arcgis.com/apps/opsdashboard/index.html",
+  referrerPolicy: "no-referrer-when-downgrade",
+  body: null,
+  method: "GET",
+  mode: "cors",
+  credentials: "omit",
+}
+).then((d) =>
+  d.json().then((e) => {
+    console.log(e);
 
-  "referrer": "https://www.arcgis.com/apps/opsdashboard/index.html",
-  "referrerPolicy": "no-referrer-when-downgrade",
-  "body": null,
-  "method": "GET",
-  "mode": "cors",
-  "credentials": "omit"
-}).then((d)=>d.json().then((e)=>{console.log(e)
+    e.features.forEach((el) => {
+      var countryRegion = el.attributes.Country_Region;
+      var Active = el.attributes.Active;
+      var Confirmed = el.attributes.Confirmed;
+      var Deaths = el.attributes.Deaths;
+      var Recovered = el.attributes.Recovered;
+      var latitude = el.attributes.Lat;
+      var longitude = el.attributes.Long_;
 
-
-e.features.forEach(element => {
-el  = element
-document.getElementById("info").innerHTML+="<br> <p id>"+el.attributes.Country_Region
-});}));
+      // id='${el.attributes.ISO3}'
+      document.getElementById(
+        "info"
+      ).innerHTML += `<br> <h1> ${countryRegion} </h1> 
+      <span class='Confirmed'>😱 Confirmed : ${Confirmed}</span><br><br>
+      <span class='Active'>😔 Active Cases : ${Active}</span><br><br>
+      <span class='Deaths'>😭 Deaths : ${Deaths}</span><br><br>
+      <span class='Recovered'>🥳 Recovered : ${Recovered}</span><br><br>
+      <span class='Relative Latitude'>   
+      <img class='latitudeImg' src="./assets/images/webimages/latitude.png" alt="" style="width: 23px;">
+      Latitude : ${latitude}
+      </span>
+      <br> <br>
+        <span class='Relative Longitude'>   <img class='longitudeImg' src="./assets/images/webimages/latitude.png" alt="" style="width: 23px;">
+      Longitude : ${longitude}
+      </span><br><br>
+         `;
+    });
+  })
+);
+var SearchInput = document.getElementById('searchInput');
