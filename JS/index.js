f95dbdcb65ffe83cda6d6171b1efe4e07d5159e4
daniya=pro <syedadaniyahaider@gmail.com>
2021-04-03 23:38:44 +0500

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
-fetch(
+var SearchInput = document.getElementById('search');
+var urlLink =fetch(
   "https://services9.arcgis.com/N9p5hsImWXAccRNI/arcgis/rest/services/Nc2JKvYFoAEOFCG5JSI6/FeatureServer/2/query?f=json&where=Recovered%3C%3E0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Recovered%20desc&resultOffset=0&resultRecordCount=250&resultType=standard&cacheHint=true", {
   referrer: "https://www.arcgis.com/apps/opsdashboard/index.html",
   referrerPolicy: "no-referrer-when-downgrade",
@@ -7,7 +8,9 @@ fetch(
   mode: "cors",
   credentials: "omit",
 }
-).then((d) =>
+);
+
+urlLink.then((d) =>
   d.json().then((e) => {
     console.log(e);
 
@@ -21,9 +24,7 @@ fetch(
       var longitude = el.attributes.Long_;
 
       // id='${el.attributes.ISO3}'
-      document.getElementById(
-        "info"
-      ).innerHTML += `<br> <h1> ${countryRegion} </h1> 
+      document.getElementById("info").innerHTML += `<br> <h1> ${countryRegion} </h1> 
       <span class='Confirmed'>😱 Confirmed : ${Confirmed}</span><br><br>
       <span class='Active'>😔 Active Cases : ${Active}</span><br><br>
       <span class='Deaths'>😭 Deaths : ${Deaths}</span><br><br>
@@ -40,5 +41,31 @@ fetch(
     });
   })
 );
-var SearchInput = document.getElementById('search');
-SearchInput.addEventListener('keydown')
+var SearchFetch =()=>{
+
+  urlLink.then((d) =>
+  d.json().then((e) => {
+    console.log(e);
+
+    e.features.forEach((el) => {
+      var countryRegion = el.attributes.Country_Region;
+      var shortForm = el.attributes.ISO3;
+      var Active = el.attributes.Active;
+      var Confirmed = el.attributes.Confirmed;
+      var Deaths = el.attributes.Deaths;
+      var Recovered = el.attributes.Recovered;
+      var latitude = el.attributes.Lat;
+      var longitude = el.attributes.Long_;
+      var Searchvalue=SearchInput.value.toLowerCase()
+      if(Searchvalue===countryRegion ||Searchvalue===shortForm){
+
+        
+
+      }
+
+}
+    )}))}
+
+
+
+SearchInput.addEventListener('keydown',SearchFetch)
