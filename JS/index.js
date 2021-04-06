fe33f1b13844e86e7bc8cb825f1285e4aa1d1625
daniya-pro <syedadaniyahaider@gmail.com>
2021-04-07 00:19:09 +0500

    updated

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,17 @@
-var SearchInput = document.getElementById("search");
-var urlLink = fetch("https://api.covid19api.com/summary");
+    var info = 
+        document.getElementById(
+          "info"
+        ); var SearchInput = document.getElementById("search");
+var urlLink =()=> fetch("https://api.covid19api.com/summary");
+var loader=document.getElementById("loading");
 
-urlLink.then((d) =>
+var container=document.getElementById("container")
+var fetchfun =()=>{
+  loader.style.display='block'
+  container.style.display='none'
+
+  info.innerHTML=''
+urlLink().then((d) =>
   d.json().then((e) => {
     console.log(e);
 
@@ -19,50 +29,81 @@ urlLink.then((d) =>
       <span class='Deaths'>😭 Deaths : ${Deaths}</span><br><br>
       <span class='Recovered'>🥳 Recovered : ${Recovered}</span><br><br>
       <span class='CC Relative'>  
-      <img class="CCImage" src="./assets/images/webimages/cc.png" alt="">
+      <img class="CCImage absolute" src="./assets/images/webimages/cc.png" alt="">
  
-     CountryCode (CC) : ${CountryCode}
+     CountryCode : ${CountryCode}
       
       </span>
       <br> <br>
       
          `;
     });
-  })
-);
+     loader.style.display='none'
+     SearchInput.value=''
+     container.style.display='block'
+ }
+)
+  );
+}
+fetchfun()
+var SearchFetch = () => {
 
-// var SearchFetch =()=>{
-//   urlLink.then((d) =>{console.log(d)}
-//   // d.json().then((e) => {
-//   console.log(e);
-//   e.features.forEach((el) => {
-//     var countryRegion = el.attributes.Country_Region;
-//     var Active = el.attributes.Active;
-//     var Confirmed = el.attributes.Confirmed;
-//     var Deaths = el.attributes.Deaths;
-//     var Recovered = el.attributes.Recovered;
-//     var latitude = el.attributes.Lat;
-//     var longitude = el.attributes.Long_;
-//   if(Searchvalue===countryRegion ||Searchvalue===shortForm){
+  loader.style.display='block'
+  container.style.display='none'
 
-//     // id='${el.attributes.ISO3}'
-//     document.getElementById("info").innerHTML += `<br> <h1> ${countryRegion} </h1>
-//     <span class='Confirmed'>😱 Confirmed : ${Confirmed}</span><br><br>
-//     <span class='Active'>😔 Active Cases : ${Active}</span><br><br>
-//     <span class='Deaths'>😭 Deaths : ${Deaths}</span><br><br>
-//     <span class='Recovered'>🥳 Recovered : ${Recovered}</span><br><br>
-//     <span class='Relative Latitude'>
-//     <img class='latitudeImg' src="./assets/images/webimages/latitude.png" alt="" style="width: 23px;">
-//     Latitude : ${latitude}
-//     </span>
-//     <br> <br>
-//       <span class='Relative Longitude'>   <img class='longitudeImg' src="./assets/images/webimages/latitude.png" alt="" style="width: 23px;">
-//     Longitude : ${longitude}
-//     </span><br><br>
-//        `;
-//   }
-//   });
-// })
-// )}
+  console.log("fetch");
+  urlLink()
+    .then((d) => d.json())
 
-// SearchInput.addEventListener('keydown',SearchFetch)
+    .then((e) => {
+      console.log(e);
+  
+             info.innerHTML=''
+
+      e.Countries.forEach((el) => {
+
+        var CountryName = el.Country;
+        var CountryCode = el.CountryCode;
+    
+
+        if (
+          SearchInput.value.toLowerCase() === CountryName.toLowerCase() || CountryCode.toLowerCase() === SearchInput.value.toLowerCase()
+        ) {
+          var Confirmed = el.TotalConfirmed;
+          var Deaths = el.TotalDeaths;
+          var Recovered = el.TotalRecovered;
+info.innerHTML += `<br> <h1> ${CountryName} </h1> 
+      <span class='Confirmed'>😱 Confirmed : ${Confirmed}</span><br><br>
+      <span class='Deaths'>😭 Deaths : ${Deaths}</span><br><br>
+      <span class='Recovered'>🥳 Recovered : ${Recovered}</span><br><br>
+      <span class='CC Relative'>  
+      <img class="CCImage absolute" src="./assets/images/webimages/cc.png" alt="">
+ 
+     CountryCode : ${CountryCode}
+      
+      </span>
+      <br><br>
+      <a class="link" onclick="fetchfun()">See Country List</a>
+      <br> <br>
+      
+         `;
+        }
+
+      })
+
+      if(info.innerHTML === ''){
+info.innerHTML =`<p class ='red' >please check if your spelling or the country code is correct and if it is then we are sorry beacaues this country isn't available yet.</p>
+<a class="link" onclick="fetchfun()">See Country List</a>
+`
+
+      }
+    });
+    setTimeout(()=>{    loader.style.display='none'
+    container.style.display='block'},1000)
+    SearchInput.value=''
+
+
+};
+
+
+document.getElementById("btn").addEventListener("click", SearchFetch);
